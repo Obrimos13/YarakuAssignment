d7dae0898bce88d93de01a6fe8651b92f7cf90b0
JLSellorsIII <jeremy.sellors@alumni.ubc.ca>
2025-03-22 16:51:35 +0900

    Book Controller Show Defined

--- src/app/Http/Controllers/BookController.php
+++ src/app/Http/Controllers/BookController.php
@@ -34,9 +34,10 @@ class BookController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Book, $book)
     {
         //
+        return response()->json($book);
     }
 
     /**
