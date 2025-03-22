d5d243f873d2fee0cdca70d8d889aa2c360e9b50
JLSellorsIII <jeremy.sellors@alumni.ubc.ca>
2025-03-22 16:45:20 +0900

    Change of User to Book reflected in relevant files,

--- src/app/Book.php
+++ src/app/Book.php
@@ -6,7 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class Book extends Authenticatable
 {
     use Notifiable;
 
@@ -16,24 +16,16 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'id', 'title', 'author',
     ];
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     * left in for future extension
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
